0abc10713fba73877aff849e5d5a3729c1542963
Jason Lynch <jason@aexoden.com>
2024-09-26 17:16:43 -0700

    Configure the display of the logger to eliminate file and line number and add elapsed time.
    Signed-off-by: Jason Lynch <jason@aexoden.com>

--- src/factortool/cli/__init__.py
+++ src/factortool/cli/__init__.py
@@ -10,7 +10,22 @@ from loguru import logger
 from factortool.config import read_config
 
 
+def setup_logger() -> None:
+    logger.remove(0)
+
+    logger_format = " | ".join([
+        "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green>",
+        "<magenta>{elapsed}</magenta>",
+        "<level>{level: <8}</level>",
+        "<level>{message}</level>",
+    ])
+
+    logger.add(sys.stdout, format=logger_format)
+
+
 def main() -> None:
+    setup_logger()
+
     try:
         _config = read_config(Path("config.json"))
     except FileNotFoundError:
