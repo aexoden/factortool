2ab8c8ce1b2ea8fba3c17a62fbcb5e0d5ce02611
Jason Lynch <jason@aexoden.com>
2024-12-22 22:20:55 -0800

    Fix the test failure after API changes.
    Signed-off-by: Jason Lynch <jason@aexoden.com>

--- tests/test_factor.py
+++ tests/test_factor.py
@@ -1,9 +1,13 @@
 # SPDX-License-Identifier: MIT
 # SPDX-FileCopyrightText: 2024 Jason Lynch <jason@aexoden.com>
 
+from pathlib import Path
+
 from factortool.number import factor_tf
+from factortool.stats import FactoringStats
 
 
 def test_tf() -> None:
     n = 15825810
-    assert factor_tf(n) == [2, 3, 5, 7, 11, 13, 17, 31]
+    stats = FactoringStats(Path("stats.json"), read_only=True)
+    assert factor_tf(n, stats) == [2, 3, 5, 7, 11, 13, 17, 31]
