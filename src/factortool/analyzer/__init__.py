3e71746011ebd8b75d45e0919a28680e64d43e82
Jason Lynch <jason@aexoden.com>
2026-02-07 21:52:03 -0800

    style: always import annotations
    Signed-off-by: Jason Lynch <jason@aexoden.com>

--- src/factortool/analyzer/__init__.py
+++ src/factortool/analyzer/__init__.py
@@ -1,3 +1,5 @@
 # SPDX-License-Identifier: MIT
 # SPDX-FileCopyrightText: 2024-2026 Jason Lynch <jason@aexoden.com>
 """Utility for analyzing factorization statistics."""
+
+from __future__ import annotations
--- src/factortool/cli/__init__.py
+++ src/factortool/cli/__init__.py
@@ -1,3 +1,5 @@
 # SPDX-License-Identifier: MIT
 # SPDX-FileCopyrightText: 2024-2026 Jason Lynch <jason@aexoden.com>
 """Utility for factoring numbers using various methods."""
+
+from __future__ import annotations
